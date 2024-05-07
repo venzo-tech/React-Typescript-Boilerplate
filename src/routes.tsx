4d81fbd8a5018cfb83740769f0fb3609e271fefb
Rahul <rahuldharmaraja0798@gmail.com>
2024-05-07 13:09:50 +0530

    routes corrections

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React, { useState, Suspense } from "react";
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from "react-router-dom";
 import { LazyLoading } from "./components/LazyLoading/Lazyloading";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Login/Register";
@@ -12,50 +12,27 @@ import { Loader } from "./Loader";
 // const Dashboard = React.lazy(() => import("./components/Dashboard"));
 
 export const RoutePagesComponent = () => {
-  const isAuthenticated = false; // Check if the user is authenticated
+  let isAuthenticated = true; // Check if the user is authenticated
   // const [isAuthenticated, setIsAuthenticated] = useState(false);
-  //   const DashboardData = LazyLoading("./components/dashboard/Dashboard");
+  // const DashboardData = LazyLoading("./components/dashboard/Dashboard");
 
   return (
     <div>
-      {/* <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <Suspense fallback={<Loader />}>
-              <Dashboard />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/protected-page"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <ProtectedPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes> */}
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          {/* <PrivateRoute isAuthenticated={isAuthenticated} element={<Dashboard />} /> */}
-          {/* {isAuthenticated ? <Route path="/dashboard" element={<Dashboard />} /> : <Navigate to="/login" />} */}
+          {/* Conditional Route with Navigate wrapped in Fragment */}
+          <Route
+            path="/dashboard"
+            element={
+              !isAuthenticated ? <Navigate to="/login" /> : <Dashboard /> // Only render Navigate if not authenticated
+            }
+          />
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </div>
   );
-};
-
-// PrivateRoute component to handle protected routes
-const PrivateRoute: React.FC<{ isAuthenticated: boolean; element: JSX.Element }> = ({
-  isAuthenticated,
-  element,
-}) => {
-  return isAuthenticated ? element : <Navigate to="/login" />;
 };
